0890e6bb100418a3af80be80eb216d40e745f879
Douglinhas <daguiaralcantara@gmail.com>
2023-12-06 09:05:23 -0300

    feat: create Calculator file

---
+++ Calculator.kt
@@ -0,0 +1,14 @@
+fun sum(a: Int, b: Int): Int = a + b;
+
+fun sub(a: Int, b: Int): Int = a - b;
+
+fun mul(a: Int, b: Int): Int = a * b;
+
+fun div(a: Double, b: Double): Double = a / b;
+
+fun main() {
+    println(sum(5, 8));
+    println(sub(5, 8));
+    println(mul(5, 8));
+    println(div(5.0, 8.0));
+}
